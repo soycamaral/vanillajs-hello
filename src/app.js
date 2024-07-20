e27f60bda301373323fe8c2a2257006df0a7ba1e
soycamaral <camaralr95@gmail.com>
2024-07-20 00:32:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 
-window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+let generateExcuse = () => {
+  let who = [
+    "The dog",
+    "My grandmother",
+    "A 4geeker",
+    "A pedestrian",
+    "The neighbour"
+  ];
+
+  let action = ["ate", "stole", "broke", "threw", "forgot", "locked"];
+
+  let what = [
+    "my sunglasses",
+    "my mobile phone",
+    "the car",
+    "my wallet",
+    "my keys",
+    "my bike"
+  ];
+
+  let when = [
+    "while I was cooking",
+    "when I was taking a shower",
+    "while I was sleeping",
+    "when I was gaming ",
+    "when I was playing the guitar"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}; 
